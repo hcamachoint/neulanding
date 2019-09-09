6a704047088108f39a41eac8496879bc28d81070
Hugo Camacho <hcamacho.int@gmail.com>
2019-09-08 22:06:16 -0400

    asd

--- footer.php
+++ footer.php
@@ -1,7 +1,15 @@
 <footer class="footer bg-footer">
 	<div class="container">
-		<?php get_sidebar('footer'); ?>
-		<span class="text-muted">&#9400; <?php bloginfo('name'); ?> Website</span>
+		<div class="row">
+			<div class="col-md-6" align="left">
+				<span class="text-muted">&#9400; <?php bloginfo('name'); ?> Website</span>
+			</div>
+			<div class="col-md-6" align="right">
+				<div style="margin-top:15px">
+					<?php get_sidebar('footer'); ?>
+				</div>
+			</div>
+		</div>
 	</div>
 </footer>
 		<?php wp_footer(); ?>
